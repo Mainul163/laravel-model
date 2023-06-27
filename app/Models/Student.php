8238eaca78b7c0ad84513eb218dfe7aaee267989
mainul163 <mainulisraf163@gmail.com>
2023-06-27 14:19:56 +0600

    first commit

---
+++ app/Models/Student.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+}
+
+
+
+// ************ jode table name singular hoi ************
+
+// protected $table = 'my_flight';
